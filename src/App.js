53ac21b764f9ba1da404e913909139cd2676a2c3
Mohammedpashaf <145536112+Mohammedpashaf@users.noreply.github.com>
2025-03-26 14:52:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './assets/css/style.css';
+import './assets/css/vendor.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import MainComponets from './componets/MainComponets';
+<><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" /><link rel="preconnect" href="https://fonts.googleapis.com" /><link rel="preconnect" href="https://fonts.gstatic.com" /><link href="https://fonts.googleapis.com/css2?family=Chilanka&family=Montserrat:wght@300;400;500&display=swap"
+  rel="stylesheet" /></>
+
+function App() {
+  return (
+    <div>
+      <MainComponets></MainComponets>
+    </div>
+  );
+}
+
+export default App;
